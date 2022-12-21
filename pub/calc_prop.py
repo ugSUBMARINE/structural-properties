2138c7238b61b40e93f83219638a3f9bd62d19dc
gwirn <71886945+gwirn@users.noreply.github.com>
2022-12-21 13:09:29 +0100

    functions instead of scripts

--- pub/calc_prop.py
+++ pub/calc_prop.py
@@ -11,6 +11,7 @@ from hydrophobic_cluster import hydr_cluster
 
 from run_rmsf_analysis import p_names
 
+
 def check_dir(path: str) -> None:
     """check if directory exists and create if it doesn't
     :parameter
@@ -23,52 +24,75 @@ def check_dir(path: str) -> None:
         os.mkdir(path)
 
 
-# ----------------------- PARAMETERS ------------------------------------
-# output directory name in pub
-parent_name = "single_struct_out"
-# structure path in pub
-struct_pub = "structures"
-# to add a protein name to the used names e.g. "769bc" - empty [] to not add
-add_names = ["N0", "769bc"]
-# -----------------------------------------------------------------------
-p_names = np.append(p_names, add_names)
+def calculate(
+    parent_name: str,
+    struct_dir: str,
+    add_names: list | None = None,
+    p_names_in: np.ndarray[tuple[int], np.dtype[str]] = None,
+):
+    """calculate properties for all given structures
+    :parameter
+        - parent_name:
+          output directory name
+        - struct_dir:
+          path to the directory containing all structures
+        - add_names:
+          to add a protein name to the used names e.g. ["769bc"]
 
-# create dir to store results
-check_dir(parent_name)
-for i in p_names:
-    # base dir for storage of data
-    i_base_path = os.path.join(parent_name, i)
-    check_dir(i_base_path)
-
-    # to be compatible with structures files
-    h_path = os.path.join(struct_pub, i + "_H.pdb")
-    if os.path.isfile(h_path):
-        file_path = h_path
+    :return
+        - func1return
+          description
+    """
+    if p_names_in is None:
+        global p_names
     else:
-        file_path = os.path.join(struct_pub, i + ".pdb")
-    print(file_path)
+        p_names = p_names_in
+    if add_names is None:
+        add_names = []
 
-    # calculate H-bonds, salt bridges, hydrophobic cluster and store their files
-    hb_path = os.path.join(i_base_path, "h_bonds")
-    check_dir(hb_path)
-    find_h_bonds(
-        file_path,
-        create_file=f"{hb_path}/{i}",
-        silent=True,
-    )
+    p_names = np.append(p_names, add_names)
 
-    sb_path = os.path.join(i_base_path, "saltbridges")
-    check_dir(sb_path)
-    find_saltbridges(
-        file_path,
-        create_file=f"{sb_path}/{i}",
-        silent=True,
-    )
+    # create dir to store results
+    check_dir(parent_name)
+    for i in p_names:
+        # base dir for storage of data
+        i_base_path = os.path.join(parent_name, i)
+        check_dir(i_base_path)
 
-    hy_path = os.path.join(i_base_path, "hydrophobic_cluster")
-    check_dir(hy_path)
-    hydr_cluster(
-        file_path,
-        create_file=f"{hy_path}/{i}",
-        silent=True,
-    )
+        # to be compatible with structures files
+        h_path = os.path.join(struct_dir, i + "_H.pdb")
+        if os.path.isfile(h_path):
+            file_path = h_path
+        else:
+            file_path = os.path.join(struct_dir, i + ".pdb")
+        print(file_path)
+
+        # calculate H-bonds, salt bridges, hydrophobic cluster and store their files
+        hb_path = os.path.join(i_base_path, "h_bonds")
+        check_dir(hb_path)
+        find_h_bonds(
+            file_path,
+            create_file=f"{hb_path}/{i}",
+            silent=True,
+        )
+
+        sb_path = os.path.join(i_base_path, "saltbridges")
+        check_dir(sb_path)
+        find_saltbridges(
+            file_path,
+            create_file=f"{sb_path}/{i}",
+            silent=True,
+        )
+
+        hy_path = os.path.join(i_base_path, "hydrophobic_cluster")
+        check_dir(hy_path)
+        hydr_cluster(
+            file_path,
+            create_file=f"{hy_path}/{i}",
+            silent=True,
+        )
+
+
+if __name__ == "__main__":
+    pass
+    calculate("test_out", "esm_double", ["N0", "769bc"])
--- pub/run_property_analysis.py
+++ pub/run_property_analysis.py
@@ -60,171 +60,215 @@ def single_stats(
     return chosen
 
 
-# ----------------------- PARAMETERS ------------------------------------
-# only possible for num_replicas > 1
-show_plots = False
-save_plots = False
-# number of structures per protein
-num_replicas = 1
-# index of which model to test and save or e.g. "!4" to specify the number of parameters
-model_ind = None  # "!6" # 1
-# how the saved model should be named - None to not save
-save_model = None  # "esm_single"  # None
-# which data the model should use for fitting
-data_path = "af_all_out"
-# to add a protein name to the used names e.g. "769bc" - empty [] to not add
-add_names = []
-# data of the added protein e.g. 75.3 - empty [] to not add
-add_temp = []
-# data to fit the model to (ndarray[tuple[int], np.dtype[int|float]])
-target = temp_cd
-# set number of attributes to choose
-# H- Bonds
-hb_param_num = 3
-# Hydrophobic Cluster
-hy_param_num = 3
-# Salt Bridges
-sb_param_num = 3
-# if chosen attributes in *_vals should be overwritten
-# replace ndarray[tuple[int], np.dtype[str]]
-ow_hb_vals = None
-ow_hy_vals = None
-ow_sb_vals = None
-# -----------------------------------------------------------------------
+def fit_and_plot(
+    data_path,
+    model_ind: int | str | None = None,
+    save_model: str | None = None,
+    num_replicas: int = 1,
+    target: np.ndarray[tuple[int], np.dtype[int | float]] = temp_cd,
+    show_plots: bool = False,
+    save_plots: bool = False,
+    add_names: list[str] | None = None,
+    add_temp: list[int] | None = None,
+    hb_param_num: int = 3,
+    hy_param_num: int = 3,
+    sb_param_num: int = 3,
+    ow_hb_vals: np.ndarray[tuple[int], np.dtype[str]] | None = None,
+    ow_hy_vals: np.ndarray[tuple[int], np.dtype[str]] | None = None,
+    ow_sb_vals: np.ndarray[tuple[int], np.dtype[str]] | None = None,
+    p_names_in: np.ndarray[tuple[int], np.dtype[str]] | None = None,
+) -> None:
+    """fit models, find the best parameter combination and plot scatter for multi data
+    per protein
+    :parameter
+        - data_path:
+          path where the data to all proteins is stored
+        - model_ind:
+          index of which model to test and save or
+          e.g. "!4" to specify the number of parameters or None to use the best model
+        - save_model:
+          how the saved model should be named - None to not save
+        - num_replicas:
+          number of structures per protein
+        - target:
+          data to fit the model to
+        - show_plots:
+          only possible for num_replicas > 1
+        - save_plots:
+          only possible for num_replicas > 1
+        - add_names:
+          to add a protein name to the used names e.g. "769bc"
+        - add_temp:
+          data of the added protein e.g. 75.3
+        - hb_param_num:
+          number of H-Bonds attributes to be tested
+        - hy_param_num:
+          number of Hydrophobic Cluster attributes to be tested
+        - sb_param_num:
+          number of Salt Bridges attributes to be tested
+        - ow_hb_vals:
+          if chosen H-Bonds attributes in *_vals should be overwritten
+        - ow_hy_vals:
+          if chosen Hydrophobic Cluster attributes in *_vals should be overwritten
+        - ow_sb_vals:
+          if chosen Salt Bridges attributes in *_vals should be overwritten
+        - p_names_in:
+          Name of the proteins and their respective files like 769bc for 769bc.pdb
+    :return
+        - None
+    """
+    if add_names is None:
+        add_names = []
+    if add_temp is None:
+        add_temp = []
+    if p_names_in is None:
+        global p_names
+    else:
+        p_names = p_names_in
 
-target = np.append(target, add_temp)
-print(" < ".join(target[np.argsort(target)].astype(str)))
-p_names = np.append(p_names, add_names)
-# name of the folders where the data is stored
-data_folders = ["saltbridges", "h_bonds", "hydrophobic_cluster"]
-data_pos = dict(zip(data_folders, np.arange(len(data_folders))))
-data = []
-for i in data_folders:
-    data.append([])
+    target = np.append(target, add_temp)
+    print(" < ".join(target[np.argsort(target)].astype(str)))
+    p_names = np.append(p_names, add_names)
+    # name of the folders where the data is stored
+    data_folders = ["saltbridges", "h_bonds", "hydrophobic_cluster"]
+    data_pos = dict(zip(data_folders, np.arange(len(data_folders))))
+    data = []
+    for i in data_folders:
+        data.append([])
 
-mulit_data = []
-for i in data_folders:
-    mulit_data.append([])
+    multi_data = []
+    for i in data_folders:
+        multi_data.append([])
 
-# for each protein
-for i in p_names:
-    # for each data attribute
-    for a in data_folders:
-        # csv dir path
-        c_path = os.path.join(data_path, i, a)
-        # all csv files
-        files = os.listdir(c_path)
-        # calculate data
-        inter_data = []
-        for c in files:
-            ac_path = os.path.join(c_path, c)
-            if a == "saltbridges":
-                sb = SaltBridges(ac_path, -1, SB_DATA_DESC)
-                inter_data.append(list(sb.stats())[:-1])
-            elif a == "h_bonds":
-                hb = SaltBridges(ac_path, -1, HB_DATA_DESC)
-                inter_data.append(list(hb.stats()[:-1]))
-            else:
-                hy = HydrophobicClusterOwn(ac_path, HY_DATA_DESC)
-                inter_data.append(list(hy.stats())[:-1])
-        # store data depending on one or multiple files per protein
-        if len(inter_data) == 1:
-            data[data_pos[a]].append(inter_data[0])
-        else:
-            mulit_data[data_pos[a]].append(inter_data)
-            data[data_pos[a]].append(np.median(inter_data, axis=0))
-
-salt_bridges_data = np.asarray(data[data_pos["saltbridges"]])
-h_bonds_data = np.asarray(data[data_pos["h_bonds"]])
-hydrophobic_cluster_data = np.asarray(data[data_pos["hydrophobic_cluster"]])
-
-print("Hydrogen Bonds")
-hb_vals = single_stats(HB_DATA_DESC, h_bonds_data, target, hb_param_num)
-print("\nHydrophobic Cluster")
-hy_vals = single_stats(HY_DATA_DESC, hydrophobic_cluster_data, target, hy_param_num)
-print("\nSalt Bridges")
-sb_vals = single_stats(SB_DATA_DESC, salt_bridges_data, target, sb_param_num)
-
-# to be able to overwrite the *_vals
-if ow_hb_vals is not None:
-    hb_vals = ow_hb_vals
-if ow_hy_vals is not None:
-    hy_vals = ow_hy_vals
-if ow_sb_vals is not None:
-    sb_vals = ow_sb_vals
-
-# create DataFrames
-sb_df = pd.DataFrame(salt_bridges_data, index=p_names, columns=SB_DATA_DESC).round(2)
-hb_df = pd.DataFrame(h_bonds_data, index=p_names, columns=HB_DATA_DESC).round(2)
-hy_df = pd.DataFrame(
-    hydrophobic_cluster_data, index=p_names, columns=HY_DATA_DESC
-).round(2)
-
-create_best_model(
-    hb_df[hb_vals],
-    hy_df[hy_vals],
-    sb_df[sb_vals],
-    hb_vals,
-    hy_vals,
-    sb_vals,
-    target,
-    p_names,
-    save_plots,
-    show_plots,
-    save_model=save_model,
-    chose_model_ind=model_ind,
-)
-
-if num_replicas > 1 and (show_plots or save_plots):
-    # can have higher indices than shown by single_stats because it shows
-    # only values which have more than one unique value
-    hb_ind = [int(np.argwhere(HB_DATA_DESC == i)) for i in hb_vals]
-    hy_ind = [int(np.argwhere(HY_DATA_DESC == i)) for i in hy_vals]
-    sb_ind = [int(np.argwhere(SB_DATA_DESC == i)) for i in sb_vals]
-
-    fig, ax = plt.subplots(
-        len(data_folders),
-        max([hb_param_num, hy_param_num, sb_param_num]),
-        figsize=(32, 18),
-    )
-    # p_names[0] = "BsPAD"
-    for i in range(len(data_folders)):
-        if data_folders[i] == "h_bonds":
-            att_inds = hb_ind
-            dn = "H- Bonds"
-            ddes = HB_DATA_DESC
-        elif data_folders[i] == "hydrophobic_cluster":
-            att_inds = hy_ind
-            dn = "Hydrophobic Cluster"
-            ddes = HY_DATA_DESC
-        elif data_folders[i] == "saltbridges":
-            att_inds = sb_ind
-            dn = "Salt Bridges"
-            ddes = SB_DATA_DESC
-        else:
-            raise KeyError("Invalid data folder encountered")
-        for p in range(len(p_names)):
-            for ca, a in enumerate(att_inds):
-                ax[i, ca].scatter(
-                    [p] * num_replicas,
-                    np.asarray(mulit_data[i][p])[:, a],
-                    label=p_names[p],
-                )
-                ax[i, ca].plot(
-                    [p - 0.2, p + 0.2],
-                    [data[i][p][a]] * 2,
-                    color="black",
-                    marker="x",
-                    linewidth=2,
-                )
-                ax[i, ca].set_title(dn)
-                ax[i, ca].set_ylabel(ddes[a])
-                if i == len(data_folders) - 1:
-                    ax[i, ca].set_xticks(np.arange(len(p_names)), p_names, rotation=45)
+    # for each protein
+    for i in p_names:
+        # for each data attribute
+        for a in data_folders:
+            # csv dir path
+            c_path = os.path.join(data_path, i, a)
+            # all csv files
+            files = os.listdir(c_path)
+            # calculate data
+            inter_data = []
+            for c in files:
+                ac_path = os.path.join(c_path, c)
+                if a == "saltbridges":
+                    sb = SaltBridges(ac_path, -1, SB_DATA_DESC)
+                    inter_data.append(list(sb.stats())[:-1])
+                elif a == "h_bonds":
+                    hb = SaltBridges(ac_path, -1, HB_DATA_DESC)
+                    inter_data.append(list(hb.stats()[:-1]))
                 else:
-                    ax[i, ca].tick_params(bottom=False, labelbottom=False)
-    fig.tight_layout(pad=5, w_pad=1.5, h_pad=1.5)
-    if save_plots:
-        fig.savefig("att_scatter.png")
-    if show_plots:
-        plt.show()
+                    hy = HydrophobicClusterOwn(ac_path, HY_DATA_DESC)
+                    inter_data.append(list(hy.stats())[:-1])
+            # store data depending on one or multiple files per protein
+            if len(inter_data) == 1:
+                data[data_pos[a]].append(inter_data[0])
+            else:
+                multi_data[data_pos[a]].append(inter_data)
+                data[data_pos[a]].append(np.median(inter_data, axis=0))
+
+    salt_bridges_data = np.asarray(data[data_pos["saltbridges"]])
+    h_bonds_data = np.asarray(data[data_pos["h_bonds"]])
+    hydrophobic_cluster_data = np.asarray(data[data_pos["hydrophobic_cluster"]])
+
+    print("Hydrogen Bonds")
+    hb_vals = single_stats(HB_DATA_DESC, h_bonds_data, target, hb_param_num)
+    print("\nHydrophobic Cluster")
+    hy_vals = single_stats(HY_DATA_DESC, hydrophobic_cluster_data, target, hy_param_num)
+    print("\nSalt Bridges")
+    sb_vals = single_stats(SB_DATA_DESC, salt_bridges_data, target, sb_param_num)
+
+    # to be able to overwrite the *_vals
+    if ow_hb_vals is not None:
+        hb_vals = ow_hb_vals
+    if ow_hy_vals is not None:
+        hy_vals = ow_hy_vals
+    if ow_sb_vals is not None:
+        sb_vals = ow_sb_vals
+
+    # create DataFrames
+    sb_df = pd.DataFrame(salt_bridges_data, index=p_names, columns=SB_DATA_DESC).round(
+        2
+    )
+    hb_df = pd.DataFrame(h_bonds_data, index=p_names, columns=HB_DATA_DESC).round(2)
+    hy_df = pd.DataFrame(
+        hydrophobic_cluster_data, index=p_names, columns=HY_DATA_DESC
+    ).round(2)
+
+    create_best_model(
+        hb_df[hb_vals],
+        hy_df[hy_vals],
+        sb_df[sb_vals],
+        hb_vals,
+        hy_vals,
+        sb_vals,
+        target,
+        p_names,
+        save_plots,
+        show_plots,
+        save_model=save_model,
+        chose_model_ind=model_ind,
+    )
+
+    if num_replicas > 1 and (show_plots or save_plots):
+        # can have higher indices than shown by single_stats because it shows
+        # only values which have more than one unique value
+        hb_ind = [int(np.argwhere(HB_DATA_DESC == i)) for i in hb_vals]
+        hy_ind = [int(np.argwhere(HY_DATA_DESC == i)) for i in hy_vals]
+        sb_ind = [int(np.argwhere(SB_DATA_DESC == i)) for i in sb_vals]
+
+        fig, ax = plt.subplots(
+            len(data_folders),
+            max([hb_param_num, hy_param_num, sb_param_num]),
+            figsize=(32, 18),
+        )
+        # p_names = [i.replace("4alb", "BsPAD") for i in p_names]
+        for i in range(len(data_folders)):
+            if data_folders[i] == "h_bonds":
+                att_inds = hb_ind
+                dn = "Hydrogen Bonds"
+                ddes = HB_DATA_DESC
+            elif data_folders[i] == "hydrophobic_cluster":
+                att_inds = hy_ind
+                dn = "Hydrophobic Cluster"
+                ddes = HY_DATA_DESC
+            elif data_folders[i] == "saltbridges":
+                att_inds = sb_ind
+                dn = "Salt Bridges"
+                ddes = SB_DATA_DESC
+            else:
+                raise KeyError("Invalid data folder encountered")
+            for p in range(len(p_names)):
+                for ca, a in enumerate(att_inds):
+                    ax[i, ca].scatter(
+                        [p] * num_replicas,
+                        np.asarray(multi_data[i][p])[:, a],
+                        label=p_names[p],
+                    )
+                    ax[i, ca].plot(
+                        [p - 0.2, p + 0.2],
+                        [data[i][p][a]] * 2,
+                        color="black",
+                        marker="x",
+                        linewidth=2,
+                    )
+                    ax[i, ca].set_title(dn)
+                    ax[i, ca].set_ylabel(ddes[a])
+                    if i == len(data_folders) - 1:
+                        ax[i, ca].set_xticks(
+                            np.arange(len(p_names)), p_names, rotation=45
+                        )
+                    else:
+                        ax[i, ca].tick_params(bottom=False, labelbottom=False)
+        fig.tight_layout(pad=5, w_pad=1.5, h_pad=1.5)
+        if save_plots:
+            fig.savefig("att_scatter.png")
+        if show_plots:
+            plt.show()
+
+
+if __name__ == "__main__":
+    pass
+    for i in [5, 6, 7]:
+        fit_and_plot("esm_d_true_out/", model_ind=f"!{i}", save_model=f"esm_d_true_{i}")
